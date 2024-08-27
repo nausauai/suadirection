5a74c5f2136193846199e9c815031554101856d0
Sakyy <sakydgny@gmail.com>
2024-08-27 10:55:20 +0700

    Add files via upload

---
+++ landing page/main.js
@@ -0,0 +1,32 @@
+const navbar = document.querySelector('#navbar');
+
+// ketika di klik memunculkan navbar
+
+function menu(){
+    navbar.classList.toggle('active');
+}
+
+// 
+window.onscroll = function() {headerShadow()};
+
+function headerShadow() {
+  const navHeader =document.getElementById("header");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
+
+  } else {
+
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
+
+  }
+}
+
+// aktif
+const aktifpage = window.Location;
+console.log(aktifpage);
